ef25cd4f2dd5845cdf5b883957b6330fff4c11cc
MarcoAerlic <100177448+MarcoAerlic@users.noreply.github.com>
2023-05-26 13:01:48 +1000

    Add files via upload

---
+++ Core.Model/CoreEntity/GPTMessage.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Model.CoreEntity
+{
+    public class GPTMessage
+    {
+        public GPTMessage(string role, string content)
+        {
+            this.Role = role;
+            this.Content = content;
+        }
+
+        [JsonProperty("role")]
+        public string Role { get; set; }
+
+        [JsonProperty("content")]
+        public string Content { get; set; }
+    }
+}
